705582b93e2e166f7868ed9345a11ea5d79ccafd
Josh Silverman <jrsilverman71@hotmail.com>
2018-06-05 10:34:20 -0700

    algos

---
+++ algorithimpractice/forloopforeach.js
@@ -0,0 +1,12 @@
+//for loop = forEach
+
+var apple = [3, 1, true, false, "hello", [1, 6], function(){console.log(5)}, 400];
+// for (var i = 0; i < apple.length; i++) {
+// 	console.log(apple[i]);
+// }
+
+apple.forEach(x => console.log(x));
+
+
+
+
