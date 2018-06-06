e09834d1240467a556fe1f7ae0083ce2411bb0b7
Josh Silverman <jrsilverman71@hotmail.com>
2018-06-06 16:19:39 -0700

    algos

--- algorithimpractice/hoisting.js
+++ algorithimpractice/hoisting.js
@@ -7,15 +7,27 @@
 // var happy = function() {
 //     return 4 + 4;
 // }
-console.log(happy()); //this call will work due to hoisting.
-var test = 7;
+// console.log(happy()); //this call will work due to hoisting.
+// var test = 7;
 
 
 
-function happy() { //this gets hoisted, moves to top of page. puts all function declarations at top of page.
-    return 4 + 4;
+// function happy() { //this gets hoisted, moves to top of page. puts all function declarations at top of page.
+//     return 4 + 4;
+// }
+
+//walk thru code as interpreter would from top down, single threadly.
+var bar = 2;
+function foo() {
+    var apple = 1;
+    zed();
+    function zed() { //this function within foo gets hoisted to top of function.
+        return 6 + 6; //this line just disappears. doing nothing with it.
+    }
+    return 2 + 2;
 }
 
 
 
+ //returns 4 in the console when you type foo(); due to HOISTING^^
 
