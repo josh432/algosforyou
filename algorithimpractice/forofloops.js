0ebc30e03cb9f744ef598bcad3a90e71958c740a
Josh Silverman <jrsilverman71@hotmail.com>
2018-06-14 13:37:03 -0700

    algos

---
+++ algorithimpractice/forofloops.js
@@ -0,0 +1,13 @@
+//for of loops
+
+var apple = "I'm so happy";
+for(let x of apple) {
+    console.log(x);
+}
+
+// can turn into an array
+var pear = "I'm so happy".split('');
+for(let x of apple) {
+    console.log(x);
+}
+
