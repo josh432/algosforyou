8d8ee6b0af2d673e2dd48ba10786baf6b4ea802d
Josh Silverman <jrsilverman71@hotmail.com>
2018-06-25 14:16:45 -0700

    algos

---
+++ algorithimpractice/spaceremover.js
@@ -0,0 +1,12 @@
+//return a string with all the spaces removed.
+//input string with spaces.
+//output string with spaces removed
+//can do this via regex.
+
+//take the string and eliminate the spaces
+//split the string to an array, join it without the spaces.
+
+var spaceRemover = function(string){
+    return string.split(" ").join("");
+}
+
