85e47c48836555e4c72233285fc2b97e24a80ba2
Josh Silverman <jrsilverman71@hotmail.com>
2018-06-28 10:47:27 -0700

    algos

--- algorithimpractice/bigO.js
+++ algorithimpractice/bigO.js
@@ -12,3 +12,16 @@ var constantExample = function(array, number) {
 var linearExample = function(array) {
     array.map(number => number + 1)
 }
+
+//quadratic time O(n^2) ex. 10 * 10 = 100, grows exponentially
+//repeat introduce to everyone in room, go back to 2nd, reintroduce,
+//go back reintroduce to 3rd, etc... very slow!! get to a point where function 
+//become unsustainable the bigger the array gets. huge exponenetial explosion
+var quadraticExample = function(arr) {
+    arr.map(num => num + arr.reduce((a,b) => a + b))
+}
+
+var orange = Array(1000).fill(1);
+console.time('a')
+quadraticExample(orange)
+console.timeEnd('a')
