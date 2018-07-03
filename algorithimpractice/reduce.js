bf417646710135c874ffc5ceb31d1d31e1522581
Josh Silverman <jrsilverman71@hotmail.com>
2018-07-02 17:53:07 -0700

    algos

--- algorithimpractice/reduce.js
+++ algorithimpractice/reduce.js
@@ -3,27 +3,34 @@
 
 var apple = [3, 4, 5, 2, 3, 2]
 //reduce under the hood
-// var reduce = function(arr, cb, a) {
-// 	var accumulatorSet = a === undefined;
-// 	arr.forEach(b => {
-// 	if(accumulatorSet){
-// 		a = b;
-// 		accumulatorSet = false;
+//cb = a + b
 
-// 		} else {
-// 			a = cb(a, b)
-// 		}
 
-// 	})
-// 	return a;
-// }
+var reduce = function(arr, cb, a) {
+	var accumulatorSet = a === undefined;
+	arr.forEach(b => {
+	if(accumulatorSet){
+		a = b;
+		accumulatorSet = false;
 
-console.log(reduce(apple, (a,b) => a + b, 400));
+		} else {
+			a = cb(a, b)
+		}
+
+	})
+	return a;
+}
+
+var maple = reduce(apple, (a, b) => a + b, 400);
+console.log(maple);
+
+
+//console.log(reduce(apple, (a,b) => a + b, 400));
 
 //how its actually used
 
 var awesome = apple.reduce(function(a, b) {
 	return a + b;
-}, 500)
+});
 
 console.log(awesome);
