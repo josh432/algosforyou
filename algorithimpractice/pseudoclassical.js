9c2a4c4cb0866a9576af07a542093acafd79c840
Josh Silverman <jrsilverman71@hotmail.com>
2018-07-05 11:19:59 -0700

    algos

---
+++ algorithimpractice/pseudoclassical.js
@@ -0,0 +1,46 @@
+//pseudo classical class instantiation 
+//es 6 has a class method
+//class = factory produces more objects you can use (constructor functions)
+//essentially, constructor function
+var Fruits = function() {
+    //{} create an empty object
+    
+    this.origin;
+    this.ripe;
+    this.price;
+    this.stock;
+}
+
+//must use keyword new
+var apple = new Fruits();
+
+apple.origin = "France";
+
+//this refers to whatever is left of the dot
+//can't do this with fat arrows
+
+var Vegetables = function(origin, ripe, price, stock) {
+    this.origin = origin;
+    this.ripe = ripe;
+    this.price = price;
+    this.stock = stock;
+}
+
+
+
+var broccoli = new Vegetables("USA", true, 1.89, 800);
+var kale = new Vegetables("Mexico", true, 1.90, 7000);
+
+//can stick functions on contructor function
+//prototypical inheritance, parents and children
+//not global in scope.
+//asks vegetable if it has function sold
+//every new vegetable we make has access to protoype
+Vegetables.prototype.sold = function() {
+    this.stock--;
+}
+
+kale.sold();
+
+
+
