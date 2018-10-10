1d559df990d28d45bc62f1fe8f71a650c7d92828
Josh Silverman <jrsilverman71@hotmail.com>
2018-10-10 10:59:49 -0700

    algos

--- algorithimpractice/addUp.js
+++ algorithimpractice/addUp.js
@@ -2,7 +2,8 @@
 //input = array, output = number
 
 //iterate over array, variable total = 0 add each number to total.
-
+//var arr = [1,2,3];
+//addUp(arr)
 var addUp = function(arr) {
     var total = 0;
     for (var i = 0; i < arr.length; i++) {
